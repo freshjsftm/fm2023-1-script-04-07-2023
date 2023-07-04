726576b817fc8ffdd89c748c2c8483911fe0ea8e
Olena Zdanovska <freshjs@gmail.com>
2023-07-04 11:08:00 +0300

    Example data type

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log(undefined);
+
+console.log(null);
+
+/*boolean*/
+console.log(true);
+console.log(false);
+
+/* string */
+console.log('true qwerty wrwrfd');
+console.log("true");
+console.log('I\'m a teacher');
+console.log("I'm a teacher");
+console.log('undefined');
+console.log('null');
+console.log('12345679878');
+
+/* numeric */
+console.log(12345679878);
+console.log(123.45679878);
+console.log(-Infinity);
+console.log(NaN);
+
+/* bigInt */
+console.log(1n);
