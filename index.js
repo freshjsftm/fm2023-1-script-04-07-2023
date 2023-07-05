9e6bc718fd54c9bb0be0a5ff5cb61833bb95e24d
Olena Zdanovska <freshjs@gmail.com>
2023-07-05 11:33:22 +0300

    Example  Assignment

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-
-let value1 = 10; //10
-let value2 = 2; //0 even  1 odd
-let result = value1 % value2;
-
+let result = (2 + 3) * 2;
 console.log(result);
 
-console.log(5%0); //NaN
-console.log(Infinity%Infinity); //NaN
+let value1 = 2;
+let value2 = 5;
+
+//value1 = value1*3; //6 = 2*3
+value1 *= 3; //6 = 2*3
+
+//value1 = value1/2; //3 = 6/2
+value1 /= 2;
+
+//value1 = value1+8; //11 = 3+8
+value1 += 8;
+
+value1 = value1 * 2 + 4;
+value1 = value1 + 2 * 4;
+
+value1 -= value2; //value1 -= 5
+
+let userName = 'Brad';
+let userSname = 'Pitt';
+userName += ' '+userSname;
