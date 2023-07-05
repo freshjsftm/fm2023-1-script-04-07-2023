3fc6f25289da971cd403d9841d46407f8ab85fd1
Olena Zdanovska <freshjs@gmail.com>
2023-07-05 10:47:34 +0300

    Example typeof

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-const AMOUNT_DAYS_OF_WEEK = 7;
-let propgramLang = 'C++';
 
-let sens = "I'm a \nteacher?";
-console.log(sens);
 
-let value1 = '12'; //12
-let value2 = null; //0
+console.log(12, typeof 12);
+console.log('qwe', typeof 'qwe');
+console.log(undefined, typeof undefined);
+console.log(true, typeof true);
 
-let result1 = value1 + value2;
-let result2 = value1 - value2;
-let result3 = value1 * value2;
-let result4 = value1 / value2;
-
-console.log('+', result1, typeof result1);
-console.log('-', result2, typeof result2);
-console.log('*', result3, typeof result3);
-console.log('/', result4, typeof result4);
+console.log(null, typeof null);
