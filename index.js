d282a2c4ce6160bb2fb1396c61f5ba48d39c3f0a
Olena Zdanovska <freshjs@gmail.com>
2023-07-05 11:45:27 +0300

    Example Exponentiation **

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-let result = (2 + 3) * 2;
-console.log(result);
 
-let value1 = 2;
-let value2 = 5;
 
-//value1 = value1*3; //6 = 2*3
-value1 *= 3; //6 = 2*3
+let value1 = -9;
+let value2 = 2;
+let value3 = 0.5;
 
-//value1 = value1/2; //3 = 6/2
-value1 /= 2;
+// Exponentiation
 
-//value1 = value1+8; //11 = 3+8
-value1 += 8;
+let result = (-value1) ** value2;
+result = -value1 * -value1;
+console.log(result)
 
-value1 = value1 * 2 + 4;
-value1 = value1 + 2 * 4;
 
-value1 -= value2; //value1 -= 5
-
-let userName = 'Brad';
-let userSname = 'Pitt';
-userName += ' '+userSname;
+let result2 = (-value1) ** value3;
+console.log(result2)
