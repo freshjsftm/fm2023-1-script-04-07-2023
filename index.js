54d39674097aaa9f10251e66fc5ed0f2271a3c9e
Olena Zdanovska <freshjs@gmail.com>
2023-07-06 10:24:48 +0300

    Example Logical NOT (!)

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const value1 = 10;
 
-// console.log(value1 == 10);
-// console.log(value1 === 10);
+/*
+  false
+  null
+  undefined
+  NaN
+  0 -0
+  '' "" ``
+*/
 
-// console.log(value1 == '10');
-// console.log(value1 === '10');
+console.log(!5); //false
+console.log(!''); //true
+console.log(!(5-5)); //true
+console.log(!' '); //false
 
-console.log(value1 != 10);
-console.log(value1 !== 10);
+console.log(!!5); //true
+console.log(!!''); //false
+console.log(!!(5-5)); //false
+console.log(!!' '); //true
 
-console.log(value1 != '10');
-console.log(Number(value1) !== Number('10'));
+console.log(Boolean(5)); //true
+console.log(Boolean('')); //false
+console.log(Boolean((5-5))); //false
+console.log(Boolean(' ')); //true
+
