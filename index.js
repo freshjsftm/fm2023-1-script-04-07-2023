74e8356e191d9013994b6b52fde1a3ade212c992
Olena Zdanovska <freshjs@gmail.com>
2023-07-06 10:10:16 +0300

    Example ==, ===, !=, !==

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-
 const value1 = 10;
 
-// console.log(value1 > 12);
-// console.log(value1 < 12);
-// console.log(value1 <= 12);
-// console.log(value1 <= 10);
+// console.log(value1 == 10);
+// console.log(value1 === 10);
 
+// console.log(value1 == '10');
+// console.log(value1 === '10');
 
-// console.log('a' > 'B');
-// console.log('abracadabra' < 'B');
-//console.log( 100 >= NaN);
+console.log(value1 != 10);
+console.log(value1 !== 10);
 
-
-// console.log(true > false);
-// console.log(true > true);
-// console.log(true >= true);
-
-
-
-
+console.log(value1 != '10');
+console.log(Number(value1) !== Number('10'));
