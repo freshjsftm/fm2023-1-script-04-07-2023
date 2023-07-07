330a257493c3bb57b03d4c33e745fbd5c594151a
Olena Zdanovska <freshjs@gmail.com>
2023-07-07 10:25:18 +0300

    Example function declaration & expression

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const userInput = prompt('Enter number', 5);
+// const userInput = prompt('Enter number', 5);
 
-console.log(userInput, typeof userInput);
-console.log(Number(userInput));
-// ''  null NaN
-// напишіть умову, яка обробляє помилку
-if (userInput === '' || userInput === null || Number.isNaN(Number(userInput))) {
-  console.log('error!');
-}else{
-  console.log('done!');
+// console.log(userInput, typeof userInput);
+// console.log(Number(userInput));
+// // ''  null NaN
+// // напишіть умову, яка обробляє помилку
+// if (userInput === '' || userInput === null || Number.isNaN(Number(userInput))) {
+//   console.log('error!');
+// }else{
+//   console.log('done!');
+// }
+
+myFirstFunction()
+
+//declaration
+function myFirstFunction(){
+  console.log('myFirstFunction');
 }
+
+myFirstFunction()
+console.log(myFirstFunction);
+
+//mySecondFunction();
+//expression
+const mySecondFunction = function (){
+  console.log('mySecondFunction');
+}
+
+mySecondFunction();
+console.log(mySecondFunction);
