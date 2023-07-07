c7b54b66d0afb8c0cb7e10942553c50bea890a79
Olena Zdanovska <freshjs@gmail.com>
2023-07-07 11:17:38 +0300

    Example return & arguments function

--- index.js
+++ index.js
@@ -10,24 +10,31 @@
 //   console.log('done!');
 // }
 
-function getSumTwoNums() {
-  const userInput1 = prompt('Enter number', 5);
-  const userInput2 = prompt('Enter number', 5);
+const userInput1 = prompt('Enter number', 5);
+const userInput2 = prompt('Enter number', 5);
 
-  const isUserInputsNotNumber =
-    userInput1 === '' ||
-    userInput1 === null ||
-    Number.isNaN(Number(userInput1)) ||
-    userInput2 === '' ||
-    userInput2 === null ||
-    Number.isNaN(Number(userInput2));
+const isUserInputsNotNumber =
+  userInput1 === '' ||
+  userInput1 === null ||
+  Number.isNaN(Number(userInput1)) ||
+  userInput2 === '' ||
+  userInput2 === null ||
+  Number.isNaN(Number(userInput2));
+  
+// let summa;  
+function getSumTwoNums(number1, number2) {
+  // const summa = Number(number1) + Number(number2);
+  // return summa;
+  return Number(number1) + Number(number2);
+}
+// summa = 100;
 
-  if (isUserInputsNotNumber) {
-    console.log('error');
-  } else {
-    const summa = Number(userInput1) + Number(userInput2);
-    console.log(userInput1, '+', userInput2, '=', summa);
-  }
+if (isUserInputsNotNumber) {
+  console.log('error');
+} else {
+  const summa = getSumTwoNums(userInput1, userInput2);
+  console.log(userInput1, '+', userInput2, '=', summa);
+  console.log(3, '+', 8, '=', getSumTwoNums(3, 8));
 }
 
-getSumTwoNums();
+
