85fbdc4b0a0841044d4974b73f31e4c685d59bbd
Olena Zdanovska <freshjs@gmail.com>
2023-07-07 11:48:17 +0300

    Add @param {number | bigint}

--- index.js
+++ index.js
@@ -13,28 +13,41 @@
 const userInput1 = prompt('Enter number', 5);
 const userInput2 = prompt('Enter number', 5);
 
+const isNotNumber = function (value) {
+  return value === '' || value === null || Number.isNaN(Number(value)); //true
+};
+
 const isUserInputsNotNumber =
-  userInput1 === '' ||
-  userInput1 === null ||
-  Number.isNaN(Number(userInput1)) ||
-  userInput2 === '' ||
-  userInput2 === null ||
-  Number.isNaN(Number(userInput2));
-  
-// let summa;  
+  isNotNumber(userInput1) || isNotNumber(userInput2);
+
+/**
+ *
+ * @param {number | bigint} number1
+ * @param {number | bigint} number2
+ * @returns {number | bigint | boolean} 
+ */
 function getSumTwoNums(number1, number2) {
-  // const summa = Number(number1) + Number(number2);
-  // return summa;
-  return Number(number1) + Number(number2);
+  if (
+    (typeof number1 === 'number' && typeof number2 === 'number') ||
+    (typeof number1 === 'bigint' && typeof number2 === 'bigint')
+  ) {
+    return number1 + number2;
+  }
+  return false;
 }
-// summa = 100;
 
 if (isUserInputsNotNumber) {
   console.log('error');
-} else {
-  const summa = getSumTwoNums(userInput1, userInput2);
+} else if (
+  Number(userInput1) > Number.MAX_SAFE_INTEGER ||
+  Number(userInput2) > Number.MAX_SAFE_INTEGER
+) {
+  const summa = getSumTwoNums(BigInt(userInput1), BigInt(userInput2));
   console.log(userInput1, '+', userInput2, '=', summa);
-  console.log(3, '+', 8, '=', getSumTwoNums(3, 8));
+} else {
+  const summa = getSumTwoNums(Number(userInput1), Number(userInput2));
+  console.log(userInput1, '+', userInput2, '=', summa);
 }
 
-
+console.log(3, '+', 8, '=', getSumTwoNums(3n, 8n));
+console.log(3n, '+', 8, '=', getSumTwoNums(3n, 8));
