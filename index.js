004075ea333ac11c99b89179acf7f0873663f048
Olena Zdanovska <freshjs@gmail.com>
2023-07-11 10:36:12 +0300

    Example Conditional (ternary) operator

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-const sideSquare = 3;
+// Conditional (ternary) operator
+// (condition)?true:false;
 
-/**
- * периметр квадрата
- * @param {number} side default=1
- * @returns {number | NaN}
- */
-const getPerimetrSquare = function (side = 1) {
-  if (side <= 0) {
-    return NaN;
-  }
-  return 4 * side;
+const logParity = function (number) {
+  return number % 2 === 0 ? 'even' : 'odd';
 };
 
-const value = -3;
-const result = getPerimetrSquare(value); //NaN
+const isEven = function (number) {
+  return number % 2 === 0;
+};
 
-if (Number.isNaN(result)) {
-  console.log('wrong argument');
-} else {
-  console.log('result = ', result);
-}
+const isAdult = function (age, AGE_ADULT = 18) {
+  return age >= AGE_ADULT;
+};
+
+//перепишіть функцію isAdult за допомогою тернарного оператора так, щоб вона повертала слово 'adult'/'not adult'
+const logAdult = function (age, AGE_ADULT = 18) {
+  return age < AGE_ADULT ? 'not adult' : 'adult';
+};
+
+const result = '' ?? 78;
+
+const promoCode = 'qwerty';
+const sale = promoCode ? 10 : 0;
+
+/*
+написати функцію, використовуючи тернарний оператор, яка запитує користувача за допомогою propmpt ввод данних, і якщо користувач будь що ввів повертає 'thank you', інакше - повертає 'error'
+- назву
+- аргументи
+- запит даних
+- перевірку
+бажано в один рядочок!
+*/
+
+const logSignNumber = function (number=0) {
+  return number > 0 ? '+' : number < 0 ? '-' : '0';
+};
+// debugger
+console.log(logSignNumber(12));
