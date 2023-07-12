4d2a7cc259aee4682c00b161ba152af3f3ca7d7f
Olena Zdanovska <freshjs@gmail.com>
2023-07-12 10:22:25 +0300

    Example clearFunction && HOF

--- index.js
+++ index.js
@@ -1,91 +1,47 @@
-//const userInputLang = prompt('Choose lang:\n1-uk\n2-en\n3-it\n4-de');
-//debugger;
-// if (
-//   userInputLang === '1' ||
-//   userInputLang === 'uk' ||
-//   userInputLang === 'ukraine'
-// ) {
-//   console.log('привіт');
-// } else if (userInputLang === '2' || userInputLang === 'en') {
-//   console.log('hi');
-// } else if (userInputLang === '3' || userInputLang === 'it') {
-//   console.log('ciao');
-// } else if (userInputLang === '4' || userInputLang === 'de') {
-//   console.log('hallo');
-// } else {
-//   console.log('try again');
-// }
-
-/*
-юзер вводить номер дня тижня - ви виводите workday || weekend
------
-юзер вводить номер місяця - ви вивoдите пору року
-1,2,12 - winter   3,4,5 - spring    6,7,8 - summer     9,10,11 - fall
-*/
-
-const userInputDayWeek = prompt('enter number day of week');
-const userInputToNumber = Number(userInputDayWeek);
-if (
-  userInputDayWeek === '' ||
-  userInputDayWeek === null ||
-  Number.isNaN(userInputToNumber) ||
-  Number.isInteger(userInputToNumber) === false ||
-  userInputToNumber < 1 ||
-  userInputToNumber > 7
-) {
-  console.log('error');
-} else if (userInputToNumber >= 1 && userInputToNumber <= 5) {
-  console.log('workday');
-} else {
-  console.log('weekend');
+const sum = function (number1, number2) {
+  return number1 + number2;
+}
+const sub = function (number1, number2) {
+  return number1 - number2;
+}
+const mul = function (number1, number2) {
+  return number1 * number2;
+}
+const dev = function (number1, number2) {
+  return number1 / number2;
+}
+/**
+ *
+ * @param {number} num1
+ * @param {number} num2
+ * @param {string} operator
+ * @returns {number | NaN}
+ */
+function calculator(number1, number2, operator) {
+  let funcAddress;
+  switch (operator) {
+    case '+':
+      funcAddress = sum; //copy address sum
+      break;
+    case '-':
+      funcAddress = sub;
+      break;
+    case '*':
+      funcAddress = mul;
+      break;
+    case '/':
+      funcAddress = dev;
+      break;
+    default:
+      return NaN;
+  }
+  return typeof funcAddress === 'function' ? funcAddress(number1, number2):NaN;
 }
 
-const userInputLang = prompt('Choose lang:\n1-uk\n2-en\n3-it\n4-de');
-switch (userInputLang) {
-  case '2':
-  case 'en':
-    console.log('hi');
-    break;
-  case '3':
-  case 'it':
-    console.log('ciao');
-    break;
-  case '4':
-  case 'de':
-    console.log('hallo');
-    break;
-  case '1':
-  case 'uk':
-  case 'UK':
-  case 'ukraine':
-    console.log('привіт');
-    break;
-  default:
-    console.log('try again'); //error
-    break;
-}
+console.log(calculator(5, 2, '/'));
 
-switch (10 > 5) {
-  case true:
-    console.log('ok');
-    break;
-  case false:
-    console.log('wrong');
-    break;
-  default:
-    break;
+function hightOrderFunction (number1, number2, funcAddress){
+  return typeof funcAddress === 'function' ? funcAddress(number1, number2):NaN;
 }
-
-// ТАК РОБИТИ НЕДОЦІЛЬНО І НЕПРАВИЛЬНО
-// const key = 50;
-// switch (key) { // key === valueCase
-//   case key>0 && key<10:
-//     console.log('ok');
-//     break;
-//   case key>10:
-//     console.log('wrong');
-//     break;
-//   default:
-//     console.log('error');
-//     break;
-// }
+debugger
+console.log(hightOrderFunction(5,6,mul));
