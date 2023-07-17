dab7a5e95c184f52427d24b5a4e861e2b9040909
Olena Zdanovska <freshjs@gmail.com>
2023-07-17 10:41:47 +0300

    Example copy address object

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min);
+const user1 = {
+  firstName: 'Brad',
+  lastName: 'Pitt',
+  age: 63,
+  isMale: true,
+};
+const user2 = {
+  firstName: 'Tom',
+  lastName: 'Rot',
+  age: 63,
+  isMale: true,
+};
+
+function hiUser(objUser){  // objUser = user1
+  objUser.age = 10;
+  return `Hi, ${objUser.firstName} ${objUser.lastName}!`;
 }
 
-const MIN = 1;
-const MAX = 10;
+console.log(hiUser(user1))
+//console.log(hiUser(user2))
 
-const checkMultTask = function () {
-  const number1 = getRandomInt(MIN, MAX);
-  const number2 = getRandomInt(MIN, MAX);
-  const result = number1 * number2;
-  while (true) {
-    //const inputUser = prompt('Enter answer:\n'+number1+' * '+number2+' = ');
-    const inputUser = prompt(`
-Enter answer:
-${number1} * ${number2} = 
-    `);
-    //check '' null  continue
-    const numberUserInput = Number(inputUser);
-    if (result === numberUserInput) {
-      //break;
-      return 'ok';
-    }
-  }
-};
-//console.log(checkMultTask());
 
-// console.log(`checkMultTask: ${checkMultTask()}`);
 
-console.log(`ternary: ${Math.random() > 0.5 ? 'good' : 'bad'}`);
+const pr1 = 3;
+const pr2 = 3;
+
+console.log(pr1 === pr2);
+
+
+const obj1 = {
+  prop:1
+}
+
+const copyObj = obj1; //копію адреси об'єкта, ярлик, alias
+copyObj.test = 'qwerty';
+
+const obj2 = {
+  prop:1
+}
+
+console.log(obj1 === obj2); //false 
+
+console.log(obj1.prop1 === obj2.prop1);
+
+console.log(obj1 === copyObj); //true 
