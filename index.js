2ad35d05db82089c80cadfa4b248aea368fdf60e
Olena Zdanovska <freshjs@gmail.com>
2023-07-18 10:23:17 +0300

    Add proptotypeMyArray

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-const arr1 = ['red', 'pink', 'aqua'];
-const arr2 = new Array('red', 'pink', 'aqua');
+const arr1 = [];
+const arr2 = [];
+//arr1[0] = 12;
+arr1.push(12);
+arr1.push(33);
+arr2.push('red');
+console.log(arr1)
+console.log(arr1.push === arr2.push); //true
 
-// const arr3 = [3];
-// const arr4 = new Array(3);
 
-const linkArr = arr1;
+const proptotypeMyArray = {};
 
-console.log(arr1 === linkArr);
-// console.log(arr1 === arr2);
-// console.log(arr1);
-// console.log(arr2);
-// console.log(arr1[1] === arr2[1]);
+proptotypeMyArray.push = function(value){
+  this[this.length] = value; 
+  this.length++;  
+  return this.length;  
+}
+proptotypeMyArray.test = function(){
+
+}
+
+function MyArray() {
+  this.length = 0;
+}
+
+MyArray.prototype = proptotypeMyArray;
+
+const arrMy1 = new MyArray(); 
+//debugger
+arrMy1.push(12); 
+arrMy1.push(33); 
+const arrMy2 = new MyArray(); 
+arrMy2.push('red'); 
+console.log(arrMy1);
+console.log(arrMy2);
+console.log(arrMy1.push === arrMy2.push); //false
