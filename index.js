93bb507985b2207187ffa782386eba2fec065678
Olena Zdanovska <freshjs@gmail.com>
2023-07-18 10:50:27 +0300

    Add ProptotypeMyArray

--- index.js
+++ index.js
@@ -4,33 +4,31 @@ const arr2 = [];
 arr1.push(12);
 arr1.push(33);
 arr2.push('red');
-console.log(arr1)
+console.log(arr1);
 console.log(arr1.push === arr2.push); //true
 
-
-const proptotypeMyArray = {};
-
-proptotypeMyArray.push = function(value){
-  this[this.length] = value; 
-  this.length++;  
-  return this.length;  
+//logic functionality
+function ProptotypeMyArray() {
+  this.push = function (value) {
+    this[this.length] = value;
+    this.length++;
+    return this.length;
+  };
+  this.test = function(){}
 }
-proptotypeMyArray.test = function(){
-
-}
-
+//data
 function MyArray() {
   this.length = 0;
 }
+// union data with logic functionality
+MyArray.prototype = new ProptotypeMyArray();
 
-MyArray.prototype = proptotypeMyArray;
-
-const arrMy1 = new MyArray(); 
+const arrMy1 = new MyArray();
 //debugger
-arrMy1.push(12); 
-arrMy1.push(33); 
-const arrMy2 = new MyArray(); 
-arrMy2.push('red'); 
+arrMy1.push(12);
+arrMy1.push(33);
+const arrMy2 = new MyArray();
+arrMy2.push('red');
 console.log(arrMy1);
 console.log(arrMy2);
 console.log(arrMy1.push === arrMy2.push); //false
