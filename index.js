076457eabb21fac868e7bb6acfb6c5657a2b4a12
Olena Zdanovska <freshjs@gmail.com>
2023-07-19 11:28:56 +0300

    Add concat empty cell

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-const array1 = [{prop:12}, {name:'qwe'}, {}];
-const array2 = ['d', 'e', 'f'];
-const array3 = array2.concat({prop:12}, 789, array2, 'qwerty');
-const array4 = array2;
 
-// console.log(array2 === array3); //shelly copy
-// console.log(array2 === array4); //copy address
-array3[0] = 'w';
-array4[0] = 'q';
+const arr = [];
 
-console.log(array3);
-// console.log(array2);
+arr[3] = 8;
+arr[8] = 3;
 
+console.log(arr)
 
-console.log(array1);
-const array5 = array1.concat(); //new address [copy address obj]
-console.log(array1[1].name);
-console.log(array5);
-console.log(array1 === array5);
-array5[1].name = 'Brad';
-console.log(array5[1].name);
-console.log(array1[1].name);
+const arr3 = arr.concat()
+arr3[2] = 5;
+console.log(arr3)
