41d008bcce32a337f261acef5b6f796eebf48c05
Olena Zdanovska <freshjs@gmail.com>
2023-07-19 12:31:38 +0300

    Add indexOf,includes,pop,shift, unshift,reverse

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+const arr = new Array(5).fill({ name: 'noname' });
+//arr.fill({name:'noname'})
+// console.log(arr);
 
-const arr = [];
+const arr2 = [55555, 2, 5, 6, 7, 88, 10000];
+const key = 5;
+// const startIndex = 1;
+console.log(arr2);
+// console.log(arr2.indexOf(key, -3))
+// let indexKey = arr2.indexOf(key, startIndex); //2
+// console.log(arr2.indexOf(key, indexKey+1)); 
+// console.log(arr2.includes(key, startIndex));
 
-arr[3] = 8;
-arr[8] = 3;
+const arr3 = arr2.reverse(); //arr3 - copy address
 
-console.log(arr)
-
-const arr3 = arr.concat()
-arr3[2] = 5;
-console.log(arr3)
+//const arr3 = arr2.concat().reverse();
+arr3[0] = 'weqwe';
+// arr2.pop(); //10000
+// arr2.shift(); //55555
+// arr2.unshift(999999);
+console.log(arr2);
+console.log(arr3);
