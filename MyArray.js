3634cfeecb0dbdda724d916d410603959cd6715b
Olena Zdanovska <freshjs@gmail.com>
2023-07-19 10:52:48 +0300

    Add method toString at MyArray

--- MyArray.js
+++ MyArray.js
@@ -5,7 +5,20 @@ function ProptotypeMyArray() {
     this.length++;
     return this.length;
   };
-  this.test = function(){}
+  this.toString = function () {
+    let stringArray = '';
+    for (let index = 0; index < this.length; index++) {
+      //v1
+      // if(index === this.length-1){
+      //   stringArray += this[index];
+      //   break;
+      // }
+      //stringArray += this[index] + ',';
+      //v2
+      stringArray += this[index] + (index === this.length - 1 ? '' : ',');
+    }
+    return stringArray;
+  };
 }
 //data
 function MyArray() {
@@ -13,3 +26,11 @@ function MyArray() {
 }
 // union data with logic functionality
 MyArray.prototype = new ProptotypeMyArray();
+
+const myArr1 = new MyArray();
+myArr1.push(12);
+myArr1.push(34);
+myArr1.push(23);
+// console.log(myArr1)
+// console.log(myArr1.toString())
+alert(myArr1);
