a5ba2788fe1ca6c58190edb316c6ce76f347acc4
Olena Zdanovska <freshjs@gmail.com>
2023-07-20 11:28:57 +0300

    Add splice

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const sliceArr1 = arr1.slice();
-const sliceArr2 = arr1.slice(-1);//arr1.length-1
-const sliceArr3 = arr1.slice(2, 7);
+// const sliceArr1 = arr1.slice();
+// const sliceArr2 = arr1.slice(-1);//arr1.length-1
+// const sliceArr3 = arr1.slice(2, 7);
 
-console.log(sliceArr1);
-console.log(sliceArr2);
-console.log(sliceArr3);
+// console.log(sliceArr1);
+// console.log(sliceArr2);
+// console.log(sliceArr3);
 
+// console.log(arr1);
+
+const start = -1;
+const deleteCount = 4;
+const spliceArr1 = arr1.splice(start, deleteCount, '+','*');
 console.log(arr1);
+console.log(spliceArr1);
