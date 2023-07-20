6b4955e8441e61526935002a8eef88c73e6959d1
Olena Zdanovska <freshjs@gmail.com>
2023-07-20 10:43:23 +0300

    Add arguments

--- MyArray.js
+++ MyArray.js
@@ -1,35 +1,57 @@
 //logic functionality
 function ProptotypeMyArray() {
-  this.push = function (value) {
-    this[this.length] = value;
-    this.length++;
+  this.push = function () {
+    for (let index = 0; index < arguments.length; index++) {
+      this[this.length] = arguments[index];
+      this.length++;
+    }
     return this.length;
   };
   this.toString = function () {
     let stringArray = '';
     for (let index = 0; index < this.length; index++) {
-      //v1
-      // if(index === this.length-1){
-      //   stringArray += this[index];
-      //   break;
-      // }
-      //stringArray += this[index] + ',';
-      //v2
-      stringArray += this[index] + (index === this.length - 1 ? '' : ',');
+      if (index === this.length - 1) {
+        stringArray += this[index];
+        break;
+      }
+      stringArray += this[index] + ',';
     }
     return stringArray;
+  };
+  this.pop = function () {
+    if (this.length === 0) {
+      return;
+    }
+    const lastElem = this[this.length - 1];
+    delete this[this.length - 1];
+    this.length--;
+    return lastElem;
+    // const lastElem = this[--this.length];
+    // delete this[this.length];
+    // return lastElem;
   };
 }
 //data
 function MyArray() {
   this.length = 0;
+  if (arguments.length === 1) {
+    for (let index = 0; index < arguments[0]; index++) {
+      this.push(undefined);
+    }
+  } else {
+    for (let index = 0; index < arguments.length; index++) {
+      this.push(arguments[index]);
+    }
+  }
 }
 // union data with logic functionality
 MyArray.prototype = new ProptotypeMyArray();
 
-// const myArr1 = new MyArray();
-// myArr1.push(12);
-// myArr1.push(34);
+const myArr1 = new MyArray(4, 8, 6);
+const myArr2 = new MyArray(3);
+myArr1.push();
+myArr1.push();
+myArr1.push(34);
 // myArr1.push(23);
 // // console.log(myArr1)
 // // console.log(myArr1.toString())
--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-const arr = new Array(5).fill({ name: 'noname' });
-//arr.fill({name:'noname'})
-// console.log(arr);
+const arr1 = new Array(7, 4, 5);
 
-const arr2 = [55555, 2, 5, 6, 7, 88, 10000];
-const key = 5;
-// const startIndex = 1;
-console.log(arr2);
-// console.log(arr2.indexOf(key, -3))
-// let indexKey = arr2.indexOf(key, startIndex); //2
-// console.log(arr2.indexOf(key, indexKey+1)); 
-// console.log(arr2.includes(key, startIndex));
-
-const arr3 = arr2.reverse(); //arr3 - copy address
-
-//const arr3 = arr2.concat().reverse();
-arr3[0] = 'weqwe';
-// arr2.pop(); //10000
-// arr2.shift(); //55555
-// arr2.unshift(999999);
-console.log(arr2);
-console.log(arr3);
+function f(num1) {
+  console.log('num1 = ', num1);
+  console.log(arguments);
+  console.log(arguments[arguments.length-1]);
+}
+//f(100,5,6);
