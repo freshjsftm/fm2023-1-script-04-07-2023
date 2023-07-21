93e94a6b02bd7583e9b9b1738f3f9ba0b5f6dd79
Olena Zdanovska <freshjs@gmail.com>
2023-07-21 11:55:58 +0300

    Add map

--- index.js
+++ index.js
@@ -1,38 +1,47 @@
-//задовільняє
+//sort
 
-//map
+// function compareNumbers(a, b) {
+//   return a - b;
+// }
 
-const arr1 = [21, 4, 8, 61];
+function compareNumbers(a, b) {
+  if (a > b) {
+    return 1; //first b then a
+  }
+  if (b > a) {
+    return -1; //first a then b
+  }
+  if (b === a) {
+    return 0; //залишаються на місцях
+  }
+}
 
-const getSquare = function (number) {
-  return number * number;
-};
-
-const arrNew = arr1.map(getSquare);
-
-const arrNew2 = arr1.map(function (elem) {
-  return { prop: elem };
-});
-
+const arr1 = [21, 400, 8, 61];
+//console.log(arr1);
+const arr2 = arr1.sort(compareNumbers);
 // console.log(arr1);
-// //console.log(arrNew);
-// console.log(arrNew2);
+// console.log(arr1 === arr2);
 
 const users = [
+  { name: 'Tom', age: 35 },
+  { name: 'Rob', age: 55 },
   { name: 'Bob', age: 45 },
-  { name: 'Tom', isMale: true },
-  { name: 'Rob' },
+  { name: 'Fred', age: 15 },
 ];
 
-const usersWithSubscribe = users.map(function (user) {
-  const newUser = {};
-  for (const key in user) {
-    newUser[key] = user[key];
+const newUsersSortByName = users.sort(function (user1, user2) {
+  if (user1.name > user2.name) {
+    return 1;
   }
-  newUser.isSubscribe = false;
-  return newUser;
+  if (user2.name > user1.name) {
+    return -1;
+  }
+  return 0;
 });
+// const newUsersSortByAge = users.sort(function(user1,user2){
+//   return user1.age - user2.age;
+// });
 
-console.log(users);
-console.log(usersWithSubscribe);
-console.log(users[0] === usersWithSubscribe[0]);
+//відсортувати юзерів
+// - за іменем
+// - за віком
