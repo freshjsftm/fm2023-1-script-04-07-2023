800ee5460f4bc2acec0c34cca53f7854b3b303a7
Olena Zdanovska <freshjs@gmail.com>
2023-07-24 10:13:57 +0300

    Add arrow function in forEach

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
 'use strict';
 
-console.log(this);
-
-//declaration
-function test1() {
-  console.log(this);
-}
-
-//expression
-const test2 = function () {
-  console.log(this);
+const site = {
+  title: 'Portfolio',
+  headers: ['Welcome', 'About me', 'Contacts'],
+  //showHeaders: function(){},
+  // showHeaders(){
+  //   //console.log(this);
+  //   this.headers.forEach(function(header){
+  //     console.log(this); //undefined
+  //     console.log(header, this.title)
+  //   })
+  // }
+  showHeaders() {
+    //console.log(this); //site
+    this.headers.forEach((header) => {
+      //console.log(this); //site
+      console.log(header, this.title);
+    });
+  },
 };
 
-//arrow
-const test3 = () => {
-  console.log(this);
-};
+site.showHeaders();
 
-test1();
-test2();
+// console.log(this);
 
-test3();
+// //declaration
+// function test1() {
+//   console.log(this);
+// }
+
+// //expression
+// const test2 = function () {
+//   console.log(this);
+// };
+
+// //arrow
+// const test3 = () => {
+//   console.log(this);
+// };
+
+// test1();
+// test2();
+
+// test3();
