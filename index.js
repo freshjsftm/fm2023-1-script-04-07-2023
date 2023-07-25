35d0b06ed7b7834233d1d32a70eeb330f609f4d4
Olena Zdanovska <freshjs@gmail.com>
2023-07-25 08:44:28 +0300

    Example ...

--- index.js
+++ index.js
@@ -1,45 +1,23 @@
 'use strict';
 
-function test1 (number, ...rest) {
-  console.log(number);
-  console.log(arguments);
-  console.log(rest);
+const sum = (...args) => {
+  let summa = 0;
+  // for (let index = 0; index < args.length; index++) {
+  //   summa += args[index];
+  // }
+  args.forEach((arg) => {
+    summa = summa + arg;
+  });
+  return summa;
 };
 
-//expression
-const test2 = function (number) {
-  console.log(number);
-  console.log(arguments);
-};
+const sumWithReduce = (...args) => args.reduce((summa, arg) => summa + arg, 0);
 
-const test22 = function (number, ...rest) {
-  console.log(number);
-  console.log(arguments);
-  console.log(rest);
-};
+const arr = [1, 2, 3, 4, 5];
+const arrNew = [...arr, 2, ...arr];
 
-//arrow
-const test3 = (number1, number2, ...args) => {
-  console.log(number1);
-  console.log(args);
-};
+const result = sum(...arr); //sum(1, 2, 3, 4, 5)
+console.log(result);
 
-test22(1, 2, 3);
-//test3(1, 2, 3, 4, 5, 6, 7);
-
-// console.dir(test2)
-// console.dir(test3)
-
-// const getAreaSquare = (number) => {
-//   return number * number;
-// };
-
-// const getAreaSquare = (number=1) => number * number;
-
-// const logAreaSquare = (number=1) => console.log(number * number);
-// // const result = logAreaSquare(5);
-// // console.log(result);
-
-// const returnObject = (value) => ({prop: value})
-// const result2 = returnObject(5);
-// console.log(result2);
+const result2 = sumWithReduce(...arr); 
+console.log(result2);
