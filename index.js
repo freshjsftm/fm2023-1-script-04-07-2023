43b608a29b85105f532abf30811210d4d15e1347
Olena Zdanovska <freshjs@gmail.com>
2023-08-04 11:32:57 +0300

    Example chainable for add()

--- index.js
+++ index.js
@@ -2,10 +2,7 @@
 
 const arr1 = [1, 5, 6, 8, 5, 5, 5];
 const set1 = new Set();
-set1.add(1);
-set1.add(arr1);
-set1.add(arr1);
-set1.add('1');
+set1.add(1).add(arr1).add(arr1).add('1');
 set1.add([1]); //new address
 set1.add([1]); //new address
 console.log(set1);
